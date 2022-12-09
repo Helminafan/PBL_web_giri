23fdeae515bdc01200b04aeb0ad263067086b5b9
helminafan <nafan2332@gmail.com>
2022-12-09 09:27:07 +0700

    where

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/store', [ApiPetugasController::class, 'store']);
+    Route::get('/indexPetugas', [ApiPetugasController::class, 'index']);
+    Route::get('/index', [NasabahController::class, 'index']);
+    Route::get('/logoutNasabah', [NasabahController::class, 'logoutNasabah']);
+    Route::get('/logoutPetugas', [ApiPetugasController::class, 'logoutPetugas']);
+});
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
