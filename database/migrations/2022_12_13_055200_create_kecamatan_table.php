8d70f58136fe24e0993e42890ef9197e589039e8
selamat maulana <setiawangobit1@gmail.com>
2022-12-13 12:55:25 +0700

    metyu1

--- database/migrations/2022_12_13_055200_create_kecamatan_table.php
+++ database/migrations/2022_12_13_055200_create_kecamatan_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('kewargaan', function (Blueprint $table) {
+        Schema::create('kecamatan', function (Blueprint $table) {
             $table->id();
             $table->string('id_kecamatan');
             $table->string('kecamatan');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kewargaan');
+        Schema::dropIfExists('kecamatan');
     }
 };
